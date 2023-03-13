68edcaced6c476802a69c1f51ff3075ddd692770
James Geneser <jamesgeneser@gmail.com>
2023-03-12 20:52:20 -0600

    .post path

--- Develop/routes/apiRoutes/NotesRouter.js
+++ Develop/routes/apiRoutes/NotesRouter.js
@@ -6,3 +6,11 @@ router.get("/notes", (req, res) => {
   let response = notesArray;
   res.json(results);
 });
+router.post("/notes", (req, res) => {
+  if (notesArray) {
+    req.body.id = notesArray.length.toString();
+  } else {
+    req.body.id = 0;
+  }
+  res.json(newNote(req.body, notesArray));
+});
