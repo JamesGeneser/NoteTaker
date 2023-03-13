6d51893919339818622ff89e694f05ecd0794a89
James Geneser <jamesgeneser@gmail.com>
2023-03-12 20:43:41 -0600

    .get pathway

--- Develop/routes/apiRoutes/NotesRouter.js
+++ Develop/routes/apiRoutes/NotesRouter.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 const { newNote } = require("../../db/notes");
 let { notesArray } = require("../../db/db.json");
+
+router.get("/notes", (req, res) => {
+  let response = notesArray;
+  res.json(results);
+});
